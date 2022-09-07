6dece598a048a654c9a99af9f7cf99086b4f6ba2
yasmin-ashraf-eshra <yasmineeshra.ye@gmail.com>
2022-09-07 18:49:34 +0200

    register student

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,38 +1,27 @@
-{{-- <form action="/create" method="POST">
+<x-app-layout>  
+<div class="form-box">       
+     <p style="font-size: 21px;font-weight: bold;">Add Student<p><br>       
+        <form method="POST" action="create">
     @csrf
-    <label for="name">Name:</label>
-    <input type="text" name="name"></br>
-    <label for="email">Email:</label>
-    <input type="text" name="email"></br>
-    <label for="password">Password:</label>
-    <input type="text" name="password"></br>
-    <button>Done</button>
-</form> --}}
-
-
     
-        
-<form method="POST" action="create">
-    @csrf
+       <!-- Name -->
+       <div>
+          <x-label for="name" :value="__('Name')" />
 
-    <!-- Name -->
-    <div>
-        <x-label for="name" :value="__('Name')" />
+          <x-input id="name" type="text" name="name"  />
+       </div>
 
-        <x-input id="name" class="block mt-1 w-full" type="text" name="name"  />
-    </div>
+       <!-- Email Address -->
+       <div class="mt-4">
+          <x-label for="email" :value="__('Email')" />
 
-    <!-- Email Address -->
-    <div class="mt-4">
-        <x-label for="email" :value="__('Email')" />
-
-        <x-input id="email" class="block mt-1 w-full" type="email" name="email"  />
-    </div>
+          <x-input id="email"  type="email" name="email"  />
+        </div>
     
-    <div class="flex items-center justify-end mt-4">
-        <x-button class="ml-4">
-            {{ __('Register') }}
-        </x-button>
-    </div>
-</form>       
+        <div class="flex items-center justify-end mt-4">
+          <x-button class="ml-4">Next</x-button>
+        </div>
+     </form> 
+</div>  
+</x-app-layout>    
     
